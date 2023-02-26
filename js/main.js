199848e99025a1d1f928105678f2ee45cf186892
Elena <lpoluesova@mail.ru>
2023-02-26 18:21:17 +0500

    Fixed bugs in file main.js

--- js/main.js
+++ js/main.js
@@ -1,24 +1,15 @@
 const randomNumber = function(from, to) {
   let result;
   if (from<0 || to<0) {
-    result = console.log('Диапазон должен быть положительным');
+    result = 'Диапазон должен быть положительным';
   } else if (to < from) {
-    result = console.log(Math.floor(Math.random() * (from - to) + to));
+    result = Math.floor(Math.random() * (from - to) + to);
     // Источник: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
   } else {
-    result = console.log(Math.floor(Math.random() * (to - from) + from));
+    result = Math.floor(Math.random() * (to - from) + from);
   }
   return result;
 };
 
-randomNumber(10, 5);
+const checkLength = (checkedString, maxLength) => checkedString.length <= maxLength;
 
-const checkLength = function(checkedString, maxLength) {
-  if (checkedString.length < maxLength) {
-    return true;
-  } else {
-    return false;
-  }
-};
-
-checkLength('lalala', 50);
